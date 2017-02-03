freedesktop/xorg-modular

commit f16477858bce32449f462bf38226923cc711d82b

release.sh: remove $MESA_VERSION from the destination location
Requested by a number of distribution maintainers. Used starting with
the 17.0 series.

v2: Keep things conditional based on the version.

Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

release.sh
@@ -574,18 +574,21 @@ process_module() {
     elif [ x"$section" = xmesa ]; then
         host_current=$host_mesa
         mesa_version=`echo $pkg_version | sed 's:-rc.*::'`
-        section_path=archive/$mesa_version
+        section_path=archive
         srv_path="/srv/$host_current/www/$section_path"
         list_to=$list_mesa_announce
         list_cc=$list_mesa_devel
 
-        # Mesa uses separate folder for each release
-        echo "Info: creating mesa directory on web server:"
-        ssh $USER_NAME$hostname mkdir -p $srv_path  &>/dev/null
-        if [ $? -ne 0 ]; then
-            echo "Error: cannot create the path \"$srv_path\" on the web server."
-            cd $top_src
-            return 1
+        # Prior to 17.0.x Mesa uses separate folder for each release
+        if test `echo $mesa_version | cut -d'.' -f1` -lt 17; then
+            section_path=$section_path/$mesa_version
+            echo "Info: creating mesa directory on web server:"
+            ssh $USER_NAME$hostname mkdir -p $srv_path  &>/dev/null
+            if [ $? -ne 0 ]; then
+                echo "Error: cannot create the path \"$srv_path\" on the web server."
+                cd $top_src
+                return 1
+            fi
         fi
     fi
 

evelikov/xorg-utils

commit 5f6fd43f0e58627732f5d1376e66e4e36f010976

release.sh: Also update srv_path for older Mesa releases.
Otherwise we'll be missing the "$version" directory and the files
will be uploaded the the wrong location.

Fixes: f16477858bc "release.sh: remove $MESA_VERSION from the
destination location"
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

release.sh
@@ -580,6 +580,7 @@ process_module() {
         # Prior to 17.0.x Mesa uses separate folder for each release
         if test `echo $mesa_version | cut -d'.' -f1` -lt 17; then
             section_path=$section_path/$mesa_version
+            srv_path="/srv/$host_current/www/$section_path"
             echo "Info: creating mesa directory on web server:"
             ssh $USER_NAME$hostname mkdir -p $srv_path  &>/dev/null
             if [ $? -ne 0 ]; then